c2e4e509c7498306653dd716f3ee97b820854594
David Brown <me@davidcbrown.io>
2017-05-20 16:49:12 -0400

    home component

---
+++ client/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+    render() {
+        return (
+            <div style={{padding: '10px'}}>Click an example above</div>
+        );
+    }
+}
+export default Home;
